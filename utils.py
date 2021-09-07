265d7e80b41444062af9d33203020b5b6fde308a
DoranLyong <cheeryun@gmail.com>
2021-09-07 20:52:35 +0900

    sortin

---
+++ utils.py
@@ -0,0 +1,48 @@
+
+import os 
+import os.path as osp 
+
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+
+
+
+def read_excel(Path:str) -> pd.core.frame.DataFrame: 
+    df = pd.read_excel(Path, index_col = 0 ) # (ref) https://pandas.pydata.org/docs/reference/api/pandas.read_excel.html
+#    df = df.dropna(axis=0)  # drop rows(axis=0) including NaN
+
+    return df
+
+
+
+
+def read_csv(Path:str) -> pd.core.frame.DataFrame: 
+    df = pd.read_csv(Path, index_col = 0, encoding='utf-8' ) # (ref) https://pandas.pydata.org/docs/reference/api/pandas.read_excel.html
+#    df = df.dropna(axis=0)  # drop rows(axis=0) including NaN
+
+    return df
+
+
+
+def vis_dataFrame(df:pd.core.frame.DataFrame, title:str, xlabel:str, ylabel:str, fmt:str='.4g'):
+    # (ref)https://datascienceparichay.com/article/get-column-names-as-list-in-pandas-dataframe/
+    col_list = df.columns.values.tolist() 
+
+    case = [] 
+    for i in col_list: 
+        arr = df[i].to_numpy().reshape(-1, 1) # vec to column array 
+        case.append(arr)
+
+    heat = np.hstack(case)
+
+    # ==== heatmap by seaborn
+    # (ref) https://seaborn.pydata.org/generated/seaborn.heatmap.html
+    # (ref) https://stackoverflow.com/questions/29647749/seaborn-showing-scientific-notation-in-heatmap-for-3-digit-numbers
+    ax = sns.heatmap(heat, annot=True, linewidths=.5, cmap="YlGnBu", fmt=fmt)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+
